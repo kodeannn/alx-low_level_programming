9d7c0c64bcd4fd26896a52d3194fc0adffdfb097
kodeannn <iankerongo123@gmail.com>
2023-11-01 22:42:50 -0700

    a program that returns the number parameters passed to a function

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - beginning of program
+ * @argc: number of parameters
+ * @argv: pointer to the string
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
